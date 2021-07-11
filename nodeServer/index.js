fad150049a56fd9c6b43103feef9d038f1116d7b
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-07-11 20:46:20 +0530

    changed the cors setting

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,12 +1,15 @@
 // Node server which will handle socket io connection
 
-const io = require('socket.io')(8000);
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: '*',
+    }
+});
 
 const users = {};
 
 io.on('connection', socket => {
     socket.on('new-user-joined', name => {
-        console.log(name, 'joined the chat');
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
